11851dc202745cc9161fa046c036522949cda804
sankalp-20 <145529569+sankalp-20@users.noreply.github.com>
2024-01-08 00:27:11 +0530

    Update Hblog.jsx

--- src/components/home/Hblog.jsx
+++ src/components/home/Hblog.jsx
@@ -10,27 +10,16 @@ const Hblog = () => {
     <>
       <section className='blog'>
         <div className='container'>
-          <Heading subtitle='Notices' title='Recent From Notices' />
+        <a href="https://drive.google.com/folderview?id=1RmsOC2D4uQANwg0VStFamy4e7hhuKqwb"><Heading subtitle='Notices' title='Recent From Notices' /></a>
           <div className='grid2'>
             {blog.slice(0, 3).map((val) => (
               <div key={val.id} className='items shadow'>
                 <div className='img'>
-                  <img src={val.cover} alt='' />
+                  {/* <a href="https://youtube.com/@sankalp-ghargharshikshakaa6348?si=pSuJXSmLXy9ToTUP"></a> */}
                 </div>
                 <div className='text'>
                   <div className='admin flexSB'>
-                    <span>
-                      <i className='fa fa-user'></i>
-                      <label htmlFor=''>{val.type}</label>
-                    </span>
-                    <span>
-                      <i className='fa fa-calendar-alt'></i>
-                      <label htmlFor=''>{val.date}</label>
-                    </span>
-                    <span>
-                      <i className='fa fa-comments'></i>
-                      <label htmlFor=''>{val.com}</label>
-                    </span>
+                  
                   </div>
                   <h1>{val.title}</h1>
                   <p>{val.desc}</p>
